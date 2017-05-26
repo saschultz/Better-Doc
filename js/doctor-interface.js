8c304106d39013020293e4e65a837b3e253e7fb8
Sara Schultz <saschultz95@gmail.com>
2017-05-26 11:19:48 -0700

    add callback function to display doctor by name from symptom and state input

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,19 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var Medical = require('./../js/doctor.js').medicalModule;
+
+var displayDoctors = function(response) {
+  response.data.forEach(function(doctor) {
+    $('.results').append(`<li>Dr. ${doctor.profile.last_name}</li>`);
+  });
+};
+
+$(document).ready(function(){
+  var currentMedicalObject = new Medical();
+  $('#find-doc').click(function(){
+    $('.results').empty();
+    var medicalIssue = $('#symptom').val();
+    var state = $('#state').val();
+    console.log(medicalIssue);
+    console.log(state);
+    currentMedicalObject.getDoctors(medicalIssue, state, displayDoctors);
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,4 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 
+Medical = function(){};
 
-exports.doctorModule = Doctor;
+Medical.prototype.getDoctors = function(medicalIssue, state, displayDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location='+state+'&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(response) {
+    displayDoctors(response);
+  })
+  .fail(function(error){
+    $('.showLocation').text(error.responseJSON.message);
+  });
+};
+
+exports.medicalModule = Medical;
